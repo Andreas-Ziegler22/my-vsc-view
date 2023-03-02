019bcbacff2ff0f8bed5a85d58267ba982d48d8a
MrBurp <andreass.ziegler@gmail.com>
2023-03-02 14:20:04 +0100

    side bar hight

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,15 +6,33 @@ export function Sidebar() {
   return (
     <div className="sidebar-container">
       <div className="side-group1 side-flex">
-        <Files size={40} />
-        <Search size={40} />
-        <GitBranch size={40} />
-        <Bug size={40} />
-        <Puzzle size={40} />
-        <Monitor size={40} />
-        <Tree size={40} />
-        <PawPrint size={40} />
-        <Lightning size={40} />
+        <a href="">
+          <Files size={40} />
+        </a>
+        <a href="">
+          <Search size={40} />
+        </a>
+        <a href="">
+          <GitBranch size={40} />
+        </a>
+        <a href="">
+          <Bug size={40} />
+        </a>
+        <a href="">
+          <Puzzle size={40} />
+        </a>
+        <a href="">
+          <Monitor size={40} />
+        </a>
+        <a href="">
+          <Tree size={40} />
+        </a>
+        <a href="">
+          <PawPrint size={40} />
+        </a>
+        <a href="">
+          <Lightning size={40} />{" "}
+        </a>
       </div>
       <div className="side-group2 side-flex">
         <User size={40} />
