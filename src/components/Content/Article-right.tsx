1e76217672c392f0f1b1643ff9edc6783bf29f8f
MrBurp <andreass.ziegler@gmail.com>
2023-03-19 20:51:00 +0100

    content right I'm happy

--- src/components/Content/Article-right.tsx
+++ src/components/Content/Article-right.tsx
@@ -5,18 +5,32 @@ export function ArticleRight() {
     <div className="article-right">
       <ul>
         <li className="Li-title">Walkthroughs</li>
-        <li className="final-art">
+        <li className="final-art test">
           <h6>Get started with VS Code</h6>
           <p>Discover the best customizations to make VS Code yours.</p>
+          <span className="bar" style={{ width: "20%" }}></span>
+        </li>
+        <li className="final-art">
+          Lear the Fundamentals
+          <br />
+          <span className="bar" style={{ width: "15%" }}></span>
+        </li>
+        <li className="final-art">
+          Boost your Productivity
+          <br />
+          <span className="bar" style={{ width: "25%" }}></span>
+        </li>
+        <li className="final-art">
+          Get stared with JavaScript and Node.js{" "}
+          <span className="update">Updated</span>
+          <br />
           <span className="bar"></span>
         </li>
-        <li className="final-art">Lear the Fundamentals</li>
-        <li className="final-art">Boost your Productivity</li>
         <li className="final-art">
-          Get stared with JavaScript and Node.js <span>Updated</span>
-        </li>
-        <li className="final-art">
-          Angular Schematics documentation <span>Updated</span>
+          Angular Schematics documentation{" "}
+          <span className="update">Updated</span>
+          <br />
+          <span className="bar" style={{ width: "10%" }}></span>
         </li>
       </ul>
     </div>
