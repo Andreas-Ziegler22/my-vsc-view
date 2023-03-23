7aed519843283e84426ef47a19d05c371bb29c94
MrBurp <andreass.ziegler@gmail.com>
2023-03-23 09:40:04 +0100

    add hook and repository component

---
+++ src/components/Content/Repos.tsx
@@ -0,0 +1,15 @@
+export function Repos() {
+  return (
+    <div className="gitRepo">
+      <ul>
+        <li>Recent</li>
+        <li>Git Repository-peojects</li>
+        <li>Git Repository-peojects</li>
+        <li>Git Repository-peojects</li>
+        <li>Git Repository-peojects</li>
+        <li>Git Repository-peojects</li>
+        <li>More..</li>
+      </ul>
+    </div>
+  );
+}
